1252e967f35974db81b3d366d0ad00b9aeca8cc0
Les Jackson <les.jackson@binarythistle.com>
2020-05-07 10:37:05 +1000

    added main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "azurerm" {
+    version = "2.5.0"
+    features {}
+}
+
+resource "azurerm_resource_group" "tf_test" {
+  name = "tfmainrg"
+  location = "Australia East"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+  name                      = "weatherapi"
+  location                  = azurerm_resource_group.tf_test.location
+  resource_group_name       = azurerm_resource_group.tf_test.name
+
+  ip_address_type     = "public"
+  dns_name_label      = "binarythistlewa"
+  os_type             =  "Linux"
+
+  container {
+      name            = "weatherapi"
+      image           = "binarythistle/weatherapi"
+        cpu             = "1"
+        memory          = "1"
+
+        ports {
+            port        = 80
+            protocol    = "TCP"
+        }
+  }
+}
