07d72657ab6bc3b9b76e65efad4538eede66df0c
Les Jackson <les.jackson@binarythistle.com>
2020-05-07 11:26:43 +1000

    added terraform to azurepipelines

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ provider "azurerm" {
     features {}
 }
 
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "tf_rg_blobstore"
+        storage_account_name = "tfstoragebinarythistle"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+}
+
 resource "azurerm_resource_group" "tf_test" {
   name = "tfmainrg"
   location = "Australia East"
