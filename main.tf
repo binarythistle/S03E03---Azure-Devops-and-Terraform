34b05af3584e739429f7ad5d22b14cb783042814
Karl Davidson <karldavidson@Karls-MBP.modem>
2021-08-27 20:18:53 +1000

    updating terraform to be simples

--- main.tf
+++ main.tf
@@ -1,47 +1,40 @@
-provider "azurerm" {
-    version = "2.5.0"
-    features {}
-}
-
 terraform {
-    backend "azurerm" {
-        resource_group_name  = "tf_rg_blobstore"
-        storage_account_name = "tfstoragebinarythistle"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.48.0"
     }
+  }
 }
 
-variable "imagebuild" {
-  type        = string
-  description = "Latest Image Build"
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
 }
-
-
 
 resource "azurerm_resource_group" "tf_test" {
   name = "tfmainrg"
   location = "Australia East"
+
+  tags={
+    environment ="terraform demo"
+  }
 }
 
-resource "azurerm_container_group" "tfcg_test" {
-  name                      = "weatherapi"
-  location                  = azurerm_resource_group.tf_test.location
-  resource_group_name       = azurerm_resource_group.tf_test.name
+resource "azurerm_virtual_network" "myterraformnetwork"{
+  name ="terraformVnet"
+  address_space =["10.0.0.0/16"]
+  location = "Australia East"
+  resource_group_name = azurerm_resource_group.tf_test.name
 
-  ip_address_type     = "public"
-  dns_name_label      = "binarythistlewa"
-  os_type             = "Linux"
-
-  container {
-      name            = "weatherapi"
-      image           = "binarythistle/weatherapi:${var.imagebuild}"
-        cpu             = "1"
-        memory          = "1"
-
-        ports {
-            port        = 80
-            protocol    = "TCP"
-        }
+  tags={
+    environment ="terraform demo"
   }
+}
+
+resource "azurerm_subnet" "myterraformsubnet"{
+  name ="terraformSubnet"
+  resource_group_name = azurerm_resource_group.tf_test.name
+  virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
+  address_prefix  = "10.0.1.0/24"
 }
